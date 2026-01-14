75e2121fc7b339134780714b7260cf74adfa40f8
Dorjster <thebuyndorj@gmail.com>
2026-01-14 11:49:52 +0800

    for deploy

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ import { OrderRouter } from "./routes/order";
 dotenv.config();
 const app = express();
 // app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 // connectDb();
 
 // app.use(router);
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import axios from "axios";
+
+export default async function Home() {
+
+
+  return (
+    <div>asd</div>
+ 
+   );
+}
